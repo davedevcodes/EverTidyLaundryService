b84e8efcef40d3c4f1e5aaef0fcf8aac27e3c994
David Brodrick <dbrodrick700@gmail.com>
2026-03-25 12:34:58 +0100

    minor changes

--- app/components/laundry-receipt.jsx
+++ app/components/laundry-receipt.jsx
@@ -18,7 +18,7 @@ const LaundryReceipt = () => {
     men: [
       { name: 'Colored Shirt', price: 800 },
       { name: 'Jean Up and Down', price: 2000 },
-      { name: 'Jeans nika', price: 500 },
+      { name: 'Jeans Shorts', price: 500 },
       { name: 'White Shirt', price: 800 },
       { name: 'Trouser', price: 800 },
       { name: 'Suit (2pcs)', price: 2500 },
